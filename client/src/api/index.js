e24b6182ad412b9d346f894c0567e5914997990c
Andie Vester <avester146@gmail.com>
2022-08-15 07:52:36 -0400

    changing url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://poetry-app-server.herokuapp.com/poem';
+const url = 'https://poetry-app-server.herokuapp.com';
 
 export const fetchPoems = () => axios.get(url);
 
