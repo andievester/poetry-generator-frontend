b802df9a04fa56241cca34ca54f892a104d8f933
Andie Vester <avester146@gmail.com>
2022-09-22 13:24:29 -0400

    fixing url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://poetry-app-server.herokuapp.com/poem/61f59268a14dceabb9ca0de7';
+const url = 'https://poetry-app-server.herokuapp.com/poem';
 
 export const fetchPoems = () => axios.get(url);
 
