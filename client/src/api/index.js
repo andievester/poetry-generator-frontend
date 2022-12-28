9c7496616cf8c6b7da5984501e2dd7a79d90fcb6
Andie Vester <avester146@gmail.com>
2022-12-28 15:30:11 -0500

    switching to correct url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://poetry-app-server.herokuapp.com/poem';
+const url = 'https://poetry-app-service.onrender.com/poem';
 
 export const fetchPoems = () => axios.get(url);
 
